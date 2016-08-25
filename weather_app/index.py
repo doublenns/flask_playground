8f208bafc2743e5ea8344f9ca1e972ca3d7b3d11
doublenns <nnamdi.anyanwu@gmail.com>
2016-08-25 10:53:01 -0400

    Added some formatting to the returned page

--- weather_app/index.py
+++ weather_app/index.py
@@ -3,6 +3,9 @@
 from flask import Flask
 import os
 import urllib2
+import json
+import time
+
 
 app = Flask(__name__)
 
@@ -18,16 +21,31 @@ def get_api_key():
     return api_key
 
 def get_weather():
+    '''
+    Makes a web query against openweathermap API for weather in Charlotte
+    '''
     api_key = get_api_key()
-    url = ("http://api.openweathermap.org/data/2.5/weather?q=charlotte,"
-        "&mode=json&units=imperial&APPID=" + api_key)
+    url = ("http://api.openweathermap.org/data/2.5/daily?q=Charlotte,"
+        "&cnt=10&mode=json&units=imperial&APPID=" + api_key)
     response = urllib2.urlopen(url).read()
     return response
 
 
 @app.route("/")
 def index():
-    return get_weather()
+    data = json.loads(get_weather())
+    page = "<html><head><title>My Weather</title></head><body>"
+    page += "<h1>Weather for {}, {}</h1>".format(data.get("city").get("name"),
+            data.get("city").get("county"))
+    for day in data.get("list"):
+        page += ("<b>date:</b> {} <b>min:</b> {} <b>max:</b> {}"
+                "<b>description</b> {} <br /> ").format(
+                        time.strftime('%d %B', time.localtime(day.get("dt"))),
+                        (day.get("temp").get("min")),
+                        day.get("temp").get("max"),
+                        day.get("weather")[0].get("description"))
+    page += "</body></html>"
+    return page
 
 
 if __name__ == '__main__':
