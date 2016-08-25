29ef8f705c0f933eab81c4990bef486b7812d6f0
doublenns <nnamdi.anyanwu@gmail.com>
2016-08-24 22:47:47 -0400

    Added a dynamic subpage

--- weather_app/index.py
+++ weather_app/index.py
@@ -13,6 +13,10 @@ def index():
 def goodbye():
     return "Goodbye, cruel world!"
 
+@app.route("/hello/<name>")
+def hello_name(name):
+    return "Hey {}! How ya doin today, buddy?".format(name)
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port, debug=True)
