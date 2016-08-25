b0c0bc9da00eb26bb73c8b53e05e145aae174b66
doublenns <nnamdi.anyanwu@gmail.com>
2016-08-24 23:49:48 -0400

    Script reads API key from file in homedir

--- weather_app/index.py
+++ weather_app/index.py
@@ -6,10 +6,21 @@ import urllib2
 
 app = Flask(__name__)
 
-# Need to create an API key to see results
+
+def get_api_key():
+    '''
+    Function reads API key from user's home directory.
+    File must be named "openweathermap.api_key"
+    '''
+    homedir = os.path.expanduser("~")
+    with open(homedir+"/openweathermap.api_key", "r") as f:
+        api_key = f.read()
+    return api_key
+
 def get_weather():
+    api_key = get_api_key()
     url = ("http://api.openweathermap.org/data/2.5/weather?q=charlotte,"
-        "&units=imperial")
+        "&mode=json&units=imperial&APPID=" + api_key)
     response = urllib2.urlopen(url).read()
     return response
 
