a25b797e91e599bead52f5a6b735b9c3add1f369
doublenns <nnamdi.anyanwu@gmail.com>
2016-08-25 21:51:14 -0400

    Added ability to use POST requests on /secure landing pg

--- weather_app/index.py
+++ weather_app/index.py
@@ -58,6 +58,60 @@ def index():
         forecast_list=forecast_list)
 
 
+#Need to make code more DRY
+@app.route("/secure")
+def secure():
+    searchcity = request.args.get("searchcity")
+    if not searchcity:
+        searchcity = "Charlotte"
+
+    data = json.loads(get_weather(searchcity))
+    try:
+        city = data["city"]["name"]
+    except KeyError:
+        return render_template("invalid_city.html", user_input = searchcity)
+    country = data["city"]["country"]
+    forecast_list = []
+
+    for d in data.get("list"):
+        day = time.strftime('%d %B', time.localtime(d.get("dt")))
+        mini = d.get("temp").get("min")
+        maxi = d.get("temp").get("max")
+        description = d.get("weather")[0].get("description")
+        forecast_list.append((day, mini, maxi, description))
+
+    return render_template("secure.html", city=city, country=country,
+        forecast_list=forecast_list)
+
+
+#Need to explicitly allow route to accept post requests
+@app.route("/weather", methods=["POST"])
+def weather():
+    #request.form instead of request.args
+    searchcity = request.form.get("searchcity")
+    if not searchcity:
+        searchcity = "Charlotte"
+
+    data = json.loads(get_weather(searchcity))
+    try:
+        city = data["city"]["name"]
+    except KeyError:
+        return render_template("sc_invalid_city.html", user_input = searchcity)
+    country = data["city"]["country"]
+    forecast_list = []
+
+    for d in data.get("list"):
+        day = time.strftime('%d %B', time.localtime(d.get("dt")))
+        mini = d.get("temp").get("min")
+        maxi = d.get("temp").get("max")
+        description = d.get("weather")[0].get("description")
+        forecast_list.append((day, mini, maxi, description))
+
+    return render_template("secure.html", city=city, country=country,
+        forecast_list=forecast_list)
+
+
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port, debug=True)
