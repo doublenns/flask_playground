0b3235bdc377721c2e85a194e748f1871bf9372b
doublenns <nnamdi.anyanwu@gmail.com>
2016-08-26 09:13:05 -0400

    Added cookies support for "unsecure" (index) site

--- weather_app/index.py
+++ weather_app/index.py
@@ -3,10 +3,12 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+from flask import make_response
 import os
 import urllib2
 import json
 import time
+import datetime
 
 '''
 TO-DO: Input validation. (Only alpha. Strip white spaces)
@@ -41,6 +43,8 @@ def get_weather(city):
 def index():
     searchcity = request.args.get("searchcity")
     if not searchcity:
+        searchcity = request.cookies.get("last_city")
+    if not searchcity:
         searchcity = "Charlotte"
 
     data = json.loads(get_weather(searchcity))
@@ -49,8 +53,8 @@ def index():
     except KeyError:
         return render_template("invalid_city.html", user_input = searchcity)
     country = data["city"]["country"]
-    forecast_list = []
 
+    forecast_list = []
     for d in data.get("list"):
         day = time.strftime('%d %B', time.localtime(d.get("dt")))
         mini = d.get("temp").get("min")
@@ -58,8 +62,12 @@ def index():
         description = d.get("weather")[0].get("description")
         forecast_list.append((day, mini, maxi, description))
 
-    return render_template("index.html", city=city, country=country,
-        forecast_list=forecast_list)
+    response = make_response(render_template("index.html", city=city,
+        country=country, forecast_list=forecast_list))
+    #Cookie won't be deleted for a year since their last visit
+    response.set_cookie("last_city","{},{}".format(city, country),
+        expires=datetime.datetime.today() + datetime.timedelta(days=365))
+    return response
 
 
 #Need to make code more DRY
