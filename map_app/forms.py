494c35bfe47c7dad91eef82da0a38fce4631088a
DoubleNNs <nnamdianyanwu@gmail.com>
2016-09-06 08:58:53 -0400

    Signup form backend

---
+++ map_app/forms.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from flask_wtf import Form
+from wtforms import Stringfield
+from wtforms import PasswordField
+from wtforms import SubmitField
+
+class SignupForm(Form):
+    first_name = StringField("First name")
+    last_name = StringField("Last name")
+    email = StringField("Email")
+    password = PasswordField("Password")
+    submit = SubmitField("")
