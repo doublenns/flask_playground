e415555c0476358a3f640bf4baba5b4a6dc1a3e9
DoubleNNs <nnamdianyanwu@gmail.com>
2016-09-06 18:40:54 -0400

    Fixed typo

--- map_app/models.py
+++ map_app/models.py
@@ -10,7 +10,7 @@ class User(db.Model):
     uid = db.Column(db.Integer, primary_key = True)
     firstname = db.Column(db.String(100))
     lastname = db.Column(db.String(100))
-    email = db.Column(db.String(120)), unique=True)
+    email = db.Column(db.String(120), unique=True)
     pwdhash = db.Column(db.String(54))
 
 
