0d7df647ff50c602e73dd8c98982c9ae8ed0a9b5
DoubleNNs <nnamdianyanwu@gmail.com>
2016-09-08 11:16:55 -0400

    Added more form validation

--- map_app/forms.py
+++ map_app/forms.py
@@ -5,23 +5,28 @@ from wtforms import StringField
 from wtforms import PasswordField
 from wtforms import SubmitField
 from wtforms.validators import DataRequired
+from wtforms.validators import Email
+from wtforms.validators import Length
 
 
 class SignupForm(Form):
     first_name = StringField("First name",
                              validators=[
-                                 DataRequired("Please enter your first name.")
+                                 DataRequired("Please enter your first name")
                                  ])
     last_name = StringField("Last name",
                             validators=[
-                                DataRequired("Please enter your last name.")
+                                DataRequired("Please enter your last name")
                                 ])
     email = StringField("Email",
                         validators=[
-                            DataRequired("Please enter your email.")
+                            DataRequired("Please enter your email"),
+                            Email("Please enter a valid e-mail address")
                             ])
     password = PasswordField("Password",
                              validators=[
-                                 DataRequired("Please enter a password.")
+                                 DataRequired("Please enter a password."),
+                                 Length(min=6, message="Passwords must be \
+                                        6 characters or longer")
                                  ])
     submit = SubmitField("Sign Up")
